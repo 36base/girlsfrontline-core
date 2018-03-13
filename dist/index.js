a068d969b1f0fe90a7bd310fe024fda246083bde
angrypepsi <angrypepsi>
2018-03-14 01:27:46 +0900

    skill

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,9 @@ var _equip = require('../data/equip.json');
 
 var _equip2 = _interopRequireDefault(_equip);
 
+var _skill = require('../data/skill.json');
+var _skill2 = _interopRequireDefault(_skill);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-module.exports = { dolls: _doll2.default, equips: _equip2.default };
+module.exports = { dolls: _doll2.default, equips: _equip2.default, skills: _skill2.default };
