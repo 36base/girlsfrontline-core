b8fac89d0f60f6e5a09d0456cffd1dfb05a5b465
CirnoV <cirnov59@gmail.com>
2018-03-20 23:32:36 +0900

    `getDoll()`이 적용되도록 수정

--- dist/index.js
+++ dist/index.js
@@ -16,14 +16,21 @@ var _fairy = require('../data/fairy.json');
 
 var _fairy2 = _interopRequireDefault(_fairy);
 
+var _doll3 = require('./api/doll');
+
+var _doll4 = _interopRequireDefault(_doll3);
+
 var _fairy3 = require('./api/fairy');
 
 var _fairy4 = _interopRequireDefault(_fairy3);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var dolls = _doll2.default.map(function (data) {
+  return (0, _doll4.default)(data);
+});
 var fairy = _fairy2.default.map(function (data) {
   return (0, _fairy4.default)(data);
 });
 
-module.exports = { dolls: _doll2.default, equips: _equip2.default, skills: _skill2.default, fairy: fairy };
+module.exports = { dolls: dolls, equips: _equip2.default, skills: _skill2.default, fairy: fairy };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import dolls from '../data/doll.json';
+import dollData from '../data/doll.json';
 import equips from '../data/equip.json';
 import skills from '../data/skill.json';
 import fairyData from '../data/fairy.json';
+import getDoll from './api/doll';
 import getFairy from './api/fairy';
 
+const dolls = dollData.map((data) => getDoll(data));
 const fairy = fairyData.map((data) => getFairy(data));
 
 module.exports = {dolls, equips, skills, fairy};
