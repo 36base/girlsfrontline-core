2453e6bc5111512ea6c0eb22f53e0d5c75ada28f
CirnoV <cirnov59@gmail.com>
2018-04-08 04:00:24 +0900

    개조 데이터 파싱

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 var _doll = require('../data/doll.json');
 
 var _doll2 = _interopRequireDefault(_doll);
@@ -23,7 +27,14 @@ var _fairy4 = _interopRequireDefault(_fairy3);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var dolls = _doll2.default.map(function (data) {
-  return (0, _doll4.default)(data);
+  var id = data.id;
+
+  // 개조 인형 데이터일때
+
+  return id > 20000 ? (0, _doll4.default)((0, _extends3.default)({}, _doll2.default.find(function (_ref) {
+    var baseId = _ref.id;
+    return baseId === id - 20000;
+  }), data)) : (0, _doll4.default)(data);
 });
 var fairy = _fairy2.default.map(function (data) {
   return (0, _fairy4.default)(data);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import fairyData from '../data/fairy.json';
 import getDoll from './api/doll';
 import getFairy from './api/fairy';
 
-const dolls = dollData.map((data) => getDoll(data));
+const dolls = dollData.map((data) => {
+  const {id} = data;
+  
+  // 개조 인형 데이터일때
+  return id > 20000
+    ? getDoll({...dollData.find(({id: baseId}) => baseId === (id - 20000)), ...data})
+    : getDoll(data);
+});
 const fairy = fairyData.map((data) => getFairy(data));
 
 module.exports = {dolls, equips, fairy};
