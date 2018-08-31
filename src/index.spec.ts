8f1d383736da99b89f197b87cca6e7d20b2aae57
CirnoV <cirnov59@gmail.com>
2018-08-31 23:15:44 +0900

    index.spec.ts 추가

---
+++ src/index.spec.ts
@@ -0,0 +1,16 @@
+import { dolls, equips, fairies, i18next } from './index';
+
+describe('entry point', () => {
+  test('`i18next` should not be undefined', () => {
+    expect(i18next).toBeTruthy();
+  });
+  test('`dolls` should not be undefined', () => {
+    expect(dolls).toBeTruthy();
+  });
+  test('`equips` should not be undefined', () => {
+    expect(equips).toBeTruthy();
+  });
+  test('`fairies` should not be undefined', () => {
+    expect(fairies).toBeTruthy();
+  });
+});
