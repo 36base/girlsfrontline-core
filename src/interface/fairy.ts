bc9bd730eb702cbeea49830c423f83de79dafed4
CirnoV <cirnov59@gmail.com>
2018-08-31 19:39:54 +0900

    IFairyGrow 인터페이스 추가, json 데이터 수정
     - critDmg -> criticalHarmRate

--- src/interface/fairy.ts
+++ src/interface/fairy.ts
@@ -1,4 +1,15 @@
+import fairyGrowJson from '../../data/fairyGrow.json';
 import { IPowerup, ISkillJson, IStats } from './base';
+
+export interface IFairyGrow {
+  [key:string]: number[];
+  armor: [number, number];
+  criticalHarmRate: [number, number];
+  dodge: [number, number];
+  hit: [number, number];
+  pow: [number, number];
+  proportion: [number, number, number, number, number];
+}
 
 export interface IFairy {
   id: number;
