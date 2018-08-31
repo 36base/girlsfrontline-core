59f6445298214abcb9af57bbe2288d38a05bac3a
CirnoV <cirnov59@gmail.com>
2018-08-31 16:56:44 +0900

    src/index.ts 수정
     - init 함수 삭제
     - exports에 i18next 추가

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,29 @@ import dollJson from '../data/doll.json';
 import equipJson from '../data/equip.json';
 import fairyJson from '../data/fairy.json';
 import Doll from './doll';
-// import Equip from './equip';
+import Equip from './equip';
+import i18next from './i18next';
 import { IDoll, IEquip, IFairy } from './interface';
 // import { getEquip } from './api/equip';
 // import { getFairy } from './api/fairy';
 
 const dollData = dollJson as IDoll[];
-// const equipData = equipJson as IEquip[];
+const equipData = equipJson as IEquip[];
 // const fairyData = fairyJson as IFairy[];
 
-interface ICore {
-  dolls:Doll[];
-}
+const dolls = dollData.map((data) => {
+  const { id } = data;
 
-function init(locale:string):ICore {
-  const dolls = dollData.map((data) => {
-    const { id } = data;
+  // 개조 인형 데이터일때
+  return id > 20000
+    ? new Doll({ ...dollData.find(({ id: dollId }) => dollId === (id - 20000)), ...data })
+    : new Doll(data);
+});
+const equips = equipData.map(data => new Equip(data));
+// TODO: TODO
+// const fairy = fairyData.map(data => getFairy(data));
 
-    // 개조 인형 데이터일때
-    return id > 20000
-      ? new Doll({ ...dollData.find(({ id: dollId }) => dollId === (id - 20000)), ...data })
-      : new Doll(data);
-  });
-  // const equips = equipData.map(data => new Equip(data));
-  // TODO: TODO
-  // const fairy = fairyData.map(data => getFairy(data));
+const gfcore = { i18next, dolls, equips };
 
-  return { dolls };
-}
-
-const gfcore = { init };
-
-export { init };
+export { i18next, dolls, equips };
 export default gfcore;
