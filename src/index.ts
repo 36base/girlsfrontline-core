c092453faa665f73ca1449b906a999a46632f9df
CirnoV <cirnov59@gmail.com>
2018-09-10 22:15:43 +0900

    export 방식 변경

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dollJson from '../data/doll.json';
 import equipJson from '../data/equip.json';
 import fairyJson from '../data/fairy.json';
+import * as api from './api';
 import Doll from './doll';
 import Equip from './equip';
 import Fairy from './fairy';
@@ -22,7 +23,4 @@ const dolls = dollData.map((data) => {
 const equips = equipData.map(data => new Equip(data));
 const fairies = fairyData.map(data => new Fairy(data));
 
-const gfcore = { i18next, dolls, equips, fairies };
-
-export { i18next, dolls, equips, fairies };
-export default gfcore;
+module.exports = { i18next, dolls, equips, fairies, api };
