f3ac88e9eedf1df05202b93b60ecf6540e526b7d
CirnoV <cirnov59@gmail.com>
2018-09-15 10:10:50 +0900

    build-locale 스크립트 개선

--- scripts/build-locale.js
+++ scripts/build-locale.js
@@ -11,6 +11,27 @@ module.exports = function (appLocales, appBuildLocales) {
       const filePath = path.join(localePath, filename);
       resources = {...resources, ...fs.readJsonSync(filePath)};
     });
+    resources = optimizeLocale(resources);
     return fs.writeFileSync(outputPath, JSON.stringify(resources));
   });
+  mergeLocale(appBuildLocales);
 };
+
+// zh-CN과 다른 언어 파일을 병합
+function mergeLocale(appBuildLocales) {
+  const locales = fs.readdirSync(appBuildLocales);
+  const defaultLocale = fs.readJsonSync(path.join(appBuildLocales, 'zh-CN.json'));
+  locales.filter((filename) => filename !== 'zh-CN.json').forEach((filename) => {
+    const filePath = path.join(appBuildLocales, filename);
+    const locale = fs.readJsonSync(filePath);
+    fs.writeFileSync(filePath, JSON.stringify({...defaultLocale, ...locale}));
+  });
+}
+
+function optimizeLocale(locale) {
+  const resources = {...locale};
+  Object.entries(resources).filter(([, value]) => value.trim() === '').forEach(([key]) => {
+    delete resources[key];
+  });
+  return resources;
+}
